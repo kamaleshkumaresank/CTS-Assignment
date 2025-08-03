c214b2465a136dabd20ece68ba7e28cb7ff76776
kamaleshkumaresank <kamaleshkumaresan88@gmail.com>
2025-08-03 12:06:12 +0530

    Deleted WEEk 2 folder inside Week 1

--- Week 1/WEEK 2/mockito-exercises/src/main/java/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-
-public class DataProcessingException extends Exception  {
-    public DataProcessingException(String message) {
-        super(message);
-    }
-
-}
--- Week 1/WEEK 2/mockito-exercises/src/main/java/ExternalApi3.java
+++
@@ -1,7 +0,0 @@
-
-public interface ExternalApi3 {
-    String getData();
-    String getDataById(int id);
-    void saveData(String data, int priority);
-
-}
